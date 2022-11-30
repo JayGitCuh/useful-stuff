359bacafd2a21285091ec05e10a4ce85aa200b59
Jay <94145603+JayGitCuh@users.noreply.github.com>
2022-11-30 02:29:17 -0800

    added support  for other new games added

--- main.lua
+++ main.lua
@@ -3,5 +3,16 @@ local github = "https://raw.githubusercontent.com/JayGitCuh/useful-stuff/main/ha
 local main = {
     ["hook"] = loadstring(game:HttpGet(github.."hook.lua"))(),
     ["visual"] = loadstring(game:HttpGet(github.."visual.lua"))(),
+    ["lua-api"] = loadstring(game:HttpGet(github.."lua-api/"))(),
+    ['supported'] = {
+        8130299583, --trident
+    };
 };
+function main:GetApi()
+    if main.supported[game.PlaceId] then
+        loadstring(game:HttpGet(main["lua-api"]..game.PlaceId..".lua"))()
+    else
+        loadstring(game:HttpGet(main["lua-api"].."universal.lua"))()
+    end
+end
 return main
