fe7e5f024bdce61871ed9b8c9f3a8ed1dc8463da
Jay <94145603+JayGitCuh@users.noreply.github.com>
2022-11-30 02:17:53 -0800

    removed and added

--- main.lua
+++ main.lua
@@ -1,1 +1,7 @@
-
+local requestFunction = request or http and http.request or syn and syn.request or false 
+local github = "https://raw.githubusercontent.com/JayGitCuh/useful-stuff/main/handler/"
+local main = {
+    ["hook"] = loadstring(game:HttpGet(github.."hook.lua")),
+    ["visual"] = loadstring(game:HttpGet(github.."visual.lua")),
+};
+return main
