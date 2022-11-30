8fefcfc93586752e624b257fd8505ab74372f24f
Jay <94145603+JayGitCuh@users.noreply.github.com>
2022-11-30 02:42:24 -0800

    added universal functions

--- handler/lua-api/universal.lua
+++ handler/lua-api/universal.lua
@@ -1,1 +1,18 @@
-print("test")
+local module = {
+};
+local character
+function module:getCharacter(player: Instance)
+    character = player.Character
+    return character, character:WaitForChild("HumanoidRootPart"), character:WaitForChild("Humanoid").Health, character:WaitForChild("Humanoid").MaxHealth
+end
+function module:getTeam(player: Instance)
+    return player.TeamColor
+end
+function module:isPlayerFriendly(player: Instance)
+    if game.Players.LocalPlayer.TeamColor ~= player.TeamColor then
+        return false
+    else
+        return true
+    end
+end
+return module;
