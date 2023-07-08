9c48d93827731d286e1959df513938453bdf3f0e
Jay <94145603+JayGitCuh@users.noreply.github.com>
2023-07-08 09:27:42 -0700

    Update vozoidextension.lua

--- vozoidextension.lua
+++ vozoidextension.lua
@@ -94,8 +94,6 @@ local classes = {
         Transparency = {1, "number"},
         Ignored = {false, "boolean"},
         Data = {"", "string"},
-        OutlineColor = {fromRGB(0, 0, 0), "Color3"},
-        Outline = {false, "boolean"},
         Size = {newUDim2(0, 100, 0, 100), "UDim2"},
         Position = {newUDim2(0, 0, 0, 0), "UDim2"}
     },
@@ -108,8 +106,6 @@ local classes = {
         Color = {fromRGB(255, 255, 255), "Color3"},
         Size = {newUDim2(0, 100, 0, 100), "UDim2"},
         Position = {newUDim2(0, 0, 0, 0), "UDim2"},
-        OutlineColor = {fromRGB(0, 0, 0), "Color3"},
-        Outline = {false, "boolean"},
         Thickness = {1, "number"},
         Filled = {true, "boolean"}
     }
